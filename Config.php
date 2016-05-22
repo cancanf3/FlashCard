299b489bc3e0a502b88dd9838b189e245a3c4897
Jose Pena <josepena@MacBook-Pro-de-Jose.local>
2016-05-22 02:03:48 -0400

    primer commit

---
+++ Config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', '107.180.41.49:3306');
+   define('DB_USERNAME', 'Flashcard35');
+   define('DB_PASSWORD', 'FlashdCard543');
+   define('DB_DATABASE', 'Flashcards1');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
