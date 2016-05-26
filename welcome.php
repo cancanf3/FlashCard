e8c463e5685f127ae0676284685813032421aafd
roska541 <eliman341@gmail.com>
2016-05-25 21:03:25 -0400

    Style & Modal

--- welcome.php
+++ welcome.php
@@ -1,10 +1,11 @@
-<html">
+<html>
    <head>
       <title>Welcome </title>
       <link rel="stylesheet" type="text/css" href="style.css">
       <!-- Latest compiled and minified CSS -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> 
       <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+      
    </head>
    
    <body> 
@@ -25,7 +26,9 @@
          echo "</div>";
          echo "<div>";
 
-         Echo "Current Categories";
+         echo "Current Categories";
+         echo "<button type='button' class='btn btn-primary btn-sm' id='buttonCat' data-toggle='modal' data-target='#myModal'>";  // Modal Button
+         echo "Register Category</button>";
          echo '<div class="container">';
          echo '<div class="row">';
 
@@ -35,25 +38,86 @@
          while( $row = mysqli_fetch_array($result) ){
             echo '<div class="col-md-3 text-center">';
             echo '<div class="box">';
-            echo '<div class="border" style="background-color:'.$color[$i].'"> <a href=""> <i class="fa fa-pencil-square-o" aria-hidden="true"> </i> </a>  <a href=""> <i id="exitIcon" class="fa fa-trash-o" aria-hidden="true"></i> </a>  </div>';
+            echo '<div class="border" style="background-color:'.$color[$i].'"> <a href=""> <i class="fa fa-pencil-square-o" aria-hidden="true"> </i> </a>  <i id="exitIcon" class="fa fa-trash-o" aria-hidden="true"></i> </div>';
             $title = $row['TITLE'];
-            echo '<div class="box-content" > <h1 class="tag-title"> ' .$title. ": </h1> ";
+            echo '<div id="box-content" class="box-content" > <h1 id="tag-title" class="tag-title">' .$title. "</h1> ";
             $description = $row['DESCRIPTION'];
             echo "<p>" .$description. "</p>";  
             //echo '<button type="button" class="btn btn-primary btn-round-lg btn-lg">  </button>';
             echo "</div> </div> </div>";
             $i++;
-            if($i > 5){ $i = 0;}
+            if($i == 5){ 
+               $i = 0;
+            }
           
          }
          echo "</div> </div> </div>";
 
-         echo "<a href='register_category.php'> \n Add a new Category</a>";
+         
+         echo "<div class='modal fade' id='myModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>";
+  		 echo  "<div class='modal-dialog' role='document'>";
+   		 echo "<div class='modal-content'>";
+      	 echo "<div class='modal-header'>";
+         echo "<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>";
+         echo "<h4 class='modal-title' id='myModalLabel'>Modal title</h4>";
+      	 echo "</div>";
+      	 echo "<div class='form-group'>
+    			<label for='inputsm'>Category Title</label>
+    			<input class='form-control input-sm' id='inputsm' type='text'>
+  			  </div>
+  			  <div class='form-group'>
+  			  	<label for='inputlg'>Description</label>
+    			<input class='form-control input-lg' id='inputlg' type='text'>
+  			</div>";
+      	 echo "<div class='modal-footer'>";
+         echo "<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>";
+         echo "<button type='button' class='btn btn-primary'>Save changes</button>";
+      	 echo "</div>";
+   		 echo "</div>";
+  		 echo "</div>" ;
+		 echo "</div>"; // End Modal */
       ?>   
       <?php echo $login_session; ?> 
-      <h2><a href = "logout.php">Sign Out</a></h2>
-      </div>
-      </div>
+      <h2><a href = "/FlashCard/Logout.php">Sign Out</a></h2>
+
    </body>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+ <script type="text/javascript"> 
+ 	$('#myModal').on('shown.bs.modal', function () {
+ 	$('#myInput').focus();
+	})
+ </script>
+<script type="text/javascript">
+   $(document).ready(function() {
+      /* Add a Category */
+      $().click(function(){
+      });
+
+
+
+
+      /* Delete Category */
+      $(".fa-trash-o").click(function() {
+
+         var title  = $(this).parent().parent().children("#box-content").children("#tag-title").text();
+         var box    = $(this).parent().parent().parent();
+         var mydata = 'DEL_CATEGORY=' + title;
+         $(this).hide();
+
+         $.ajax( {
+            type: 'POST',
+            url: '/FlashCard/respond.php',
+            data: mydata,
+            success:function(respond) {
+               box.fadeOut();
+            }
+         });
+      });
+
+
+   });   
+</script>
    
 </html>
+
