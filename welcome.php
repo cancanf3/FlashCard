9a918b5d521438cfb2f39dfe545de3f01b30d6ef
Eliecer Vera <eliecervera@Eliecers-MacBook-Pro.local>
2016-05-29 23:39:15 -0400

    Try2

--- welcome.php
+++ welcome.php
@@ -24,7 +24,7 @@
 
          echo "<h1> Welcome ".$_SESSION['login_user']."</h1>";
          echo "</div>";
-         echo "<div class='FLashCardDiv'>";
+         echo "<div class='FLashCardDiv' id='FLashCardDiv'>";
          echo "<div class='SubTitle'>";
          echo "<p> Current Categories </p> ";
          echo "<button type='button' class='btn btn-primary btn-sm' id='buttonCat' data-toggle='modal' data-target='#myModal'>";  // Modal Button
@@ -40,7 +40,7 @@
             echo '<div class="box">';
             echo '<div class="border" style="background-color:'.$color[$i].'"> <i class="fa fa-pencil-square-o" aria-hidden="true" data-toggle="modal" data-target="#myModal"> </i>  <i id="exitIcon" class="fa fa-trash-o" aria-hidden="true"></i> </div>';
             $title = $row['TITLE'];
-            echo '<div id="box-content" class="box-content" > <a id="changePage"href="category.php"> <h1 id="tag-title" class="tag-title">' .$title. "</h1> </a> ";
+            echo '<div id="box-content" class="box-content" > <a href="#" id="changePage"> <h1 id="tag-title" class="tag-title">' .$title. "</h1> </a> ";
             $description = $row['DESCRIPTION'];
             echo "<p id='tag-description'>" .$description. "</p>";  
             //echo '<button type="button" class="btn btn-primary btn-round-lg btn-lg">  </button>';
@@ -148,9 +148,8 @@
 
       /* Open Category */
       $(document).ready(function(){
-        $('#changePage').click(function(e){
-          e.preventDefault();
-          $("#FlahCardDyn").load($(this).attr('href'));
+        $("#changePage").on("click", function() {
+        $("#FLashCardDiv").load("category.php");
         });
       });
 
